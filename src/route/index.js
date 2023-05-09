5c5eb5308c201f6c097197775d71d88354647db6
chip22294 <chip22294@gmail.com>
2023-05-09 15:17:36 +0300

    The lesson is over

--- src/route/index.js
+++ src/route/index.js
@@ -4,8 +4,37 @@ const router = express.Router()
 router.get('/', function (req, res) {
   res.render('index', {
     layout: 'default',
-    title: 'Home page',
+    title: 'My Home page',
     text: 'Click me',
+  })
+})
+
+router.get('/ul', function (req, res) {
+  res.render('user-list', {
+    layout: 'header',
+    title: 'User list page',
+    data: {
+      userList: [
+        {
+          id: 5123,
+          name: 'Ivan',
+          isAdmin: true,
+          balance: 5,
+        },
+        {
+          id: 4598,
+          name: 'Sasha',
+          isAdmin: false,
+          balance: 0,
+        },
+        {
+          id: 1358,
+          name: 'Kolya',
+          isAdmin: true,
+          balance: null,
+        },
+      ],
+    },
   })
 })
 
